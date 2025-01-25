6e7e68f727bedc85aa1fb496161579bb6611f7bc
Chika Chang <13810485+chikacc@users.noreply.github.com>
2025-01-25 18:15:12 +0800

    feat: add start scene

---
+++ src/GGJ2025TeamD/Assets/Scripts/SceneManagement/SceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    [SceneName]
+    public string SceneName;
+
+    public void Load()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
