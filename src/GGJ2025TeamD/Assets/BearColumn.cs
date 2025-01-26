0ae4efb20f403fa70f6591f91527653c9835de1c
AnyueDontSnipe <azhi11003@gmail.com>
2025-01-26 13:53:50 +0800

    SFX

--- src/GGJ2025TeamD/Assets/BearColumn.cs
+++ src/GGJ2025TeamD/Assets/BearColumn.cs
@@ -10,6 +10,7 @@ public class BearColumn : MonoBehaviour
     private float flowValue =0f;
     [SerializeField]
     private float maxWidth = 0.3f;
+    private bool isPlayingSound;
     private void Start()
     {
     }
@@ -17,6 +18,16 @@ public class BearColumn : MonoBehaviour
     private void Update()
     {
         flowValue = curve.Evaluate(layer.eulerAngles.z);
+        if (flowValue > 0 && !isPlayingSound)
+        {
+            AudioManager.PlaySound(SoundType.BEER_WATER);
+            isPlayingSound = true;
+        }
+        if (flowValue <= 0)
+        {
+            AudioManager.StopSound(SoundType.BEER_WATER);
+            isPlayingSound = false;
+        }
         transform.localScale = new Vector3(maxWidth * flowValue, transform.localScale.y, transform.localScale.z);
         GlobalEvent.RaiseFlowUpdate(flowValue);
     }
